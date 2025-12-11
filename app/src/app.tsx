b7388fb88bedd69ec7e94a9fc710f5a2c59399f1
misebox <misebox@gmail.com>
2025-12-11 14:20:07 +0900

    setup

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,13 +1,12 @@
 import { Suspense } from "solid-js";
-import { css } from '../styled-system/css'
 
 import "./app.css";
 
 export default function App() {
   return (
     <Suspense>
-      <div class={css({ fontSize: '2xl', fontWeight: 'bold' })}>
-        Hello 🐼!
+      <div class="">
+        Hello !
       </div>
     </Suspense>
   );
