608912bca34725836658a2423e4186db3730cbce
misebox <misebox@gmail.com>
2025-12-11 20:50:46 +0900

    Update app title

--- app/src/components/app/Header.tsx
+++ app/src/components/app/Header.tsx
@@ -4,7 +4,7 @@ export const Header: Component = () => {
   return (
     <header class="text-center py-2">
       <h1 class="text-xl font-normal text-white drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">
-        AA
+        AA denwa
       </h1>
     </header>
   );
