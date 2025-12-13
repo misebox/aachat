713e1ec87f4c7b9f9fbafb84d859485f4b3a3e09
misebox <misebox@gmail.com>
2025-12-14 07:36:50 +0900

    adjust

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -132,9 +132,9 @@ export const HomePage = () => {
       <StatusBar variant="desktop" />
 
       {/* Main content area - flex to push content down */}
-      <div class="flex flex-col justify-around gap-16">
+      <div class="flex flex-col justify-around gap-8 mt-10">
         {/* Tagline - above video area */}
-        <div class="text-center mt-10 py-2 text-gray-400 text-sm">
+        <div class="text-center py-2 text-gray-400 text-sm">
           <Typewriter
             class="text-base"
             text={descriptionTagline}
